88a210a97ed90023b53a00d8d3ca370044ee83fd
AshwinHSACodes <ashwinhsacodes@gmail.com>
2022-03-03 15:14:05 +0530

    Add controller methods 1.To accept path variables. 2.To accept query parameters.

--- SpringBoot-BackEnd/0-basic-springboot-application/src/main/java/com/basicapplication/GreetingController.java
+++ SpringBoot-BackEnd/0-basic-springboot-application/src/main/java/com/basicapplication/GreetingController.java
@@ -1,13 +1,35 @@
 package com.basicapplication;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "greeting")
+@RestController
+@RequestMapping(path = "/basicapp/api/v0/")
 public class GreetingController {
 
-	@GetMapping(value = "/greeting")
+	@GetMapping(value = "/greetingget")
 	public String greetings() {
-		return "Hello , Welcome to spring boot";
+		return "Hello , Welcome to spring boot : This is get method call.";
 	}
+	
+	@GetMapping(value = "/greetingpathvariable/{input}")
+	public String greetingsWithPathVariable(@PathVariable("input") String inputVariable) {
+		return "Hello , Welcome to spring boot : This is post method call with pathvariable : " + inputVariable + ".";
+	}
+	
+	@GetMapping(value = "/greetinrequestparameter")
+	public String greetingsPost(@RequestParam String requestParameter) {
+		return "Hello , Welcome to spring boot : This is post method call : " + requestParameter + "."  ;
+	}
+	
+	@PostMapping(value = "/greetingpost")
+	public String greetingsPostQuery() {
+		return "Hello , Welcome to spring boot : This is post method call.";
+	}
+		
 }
+
